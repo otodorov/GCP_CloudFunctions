fe8bb3454459af32ce4163dde9c89dfbf7035399
Ognyan Todorov <ognyan.tod@gmail.com>
2022-02-21 16:30:48 +0200

    Fix resize_gke_node_pool function

--- gke-list_clusters.py
+++ gke-list_clusters.py
@@ -3,12 +3,19 @@ from google.cloud import container_v1
 # http://localhost:8080?project=anakatech&zone=europe-west1-d
 
 project = 'netomedia2'
-label = ('env', 'qa')
+# label = ('env', 'qa')
+label = ('scale', 'true')
+node_count = 0
 
 client = container_v1.ClusterManagerClient()
 
 
-def list_gke_clusters(client):
+def list_gke_clusters():
+    f'''
+    List all clusters with labels and their node pools as a map that have specific label {label}
+    e.g.: {{'<cluster_name>': [['<region>'], {{'key1': 'val1', 'key2': 'val2'}}, '<node_pool_name>']}}
+    '''
+
     request = container_v1.ListClustersRequest(
         parent=f"projects/{project}/locations/-"
     )
@@ -18,19 +25,45 @@ def list_gke_clusters(client):
     cluster_dict = {}
     for cluster in response.clusters:
         if (label) in cluster.resource_labels.items():
-            cluster_dict[cluster.name] = [
-                cluster.locations,
-                cluster.resource_labels
-            ]
+            cluster_dict[cluster.name] = {
+                "location": cluster.locations,
+                "labels": cluster.resource_labels,
+                "node_pool": [node_pool.name for node_pool in cluster.node_pools],
+            }
 
     return cluster_dict
 
 
-def main():
-    print(list_gke_clusters(client))
+list_gke_clusters()
 
-    # for i in list_gke_clusters(client):
-    #   print(f"TEST - {i}")
+
+def resize_gke_node_pool(cluster: tuple, node_count: int):
+    '''
+    Params:
+        cluster: (
+            '<cluster_name>', {
+                'location': ['<locations>'],
+                'labels': {'<key1>': '<value1>', '<key2>': '<value2>'},
+                'node_pool': ['<node_pool_1>', '<node_pool_2>']
+                }
+            )
+    name string format: "projects/<project>/locations/<location>/clusters/<cluster_name>/nodePools/<node_pool>"
+    '''
+
+    for node_pool in cluster[1]['node_pool']:
+        request = container_v1.SetNodePoolSizeRequest(
+            name=f"projects/{project}/locations/{cluster[1]['location'][0]}/clusters/{cluster[0]}/nodePools/{node_pool}",
+            node_count=node_count,
+        )
+
+    response = client.set_node_pool_size(request=request)
+
+    return response
+
+
+def main():
+    for cluster in list_gke_clusters().items():
+        resize_gke_node_pool(cluster, node_count)
 
 
 main()
--- gke-list_node_pools.py
+++ gke-list_node_pools.py
@@ -1,5 +1,7 @@
 from google.cloud import container_v1
 
+# http://localhost:8080?project=anakatech&zone=europe-west1-d
+
 project = 'netomedia2'
 label = ('env', 'qa')
 
@@ -18,31 +20,17 @@ def list_gke_clusters(client):
         if (label) in cluster.resource_labels.items():
             cluster_dict[cluster.name] = [
                 cluster.locations,
-                cluster.resource_labels
+                cluster.resource_labels,
+                # cluster.node_pools[0].name,
+                cluster.node_pools,
+                # cluster.node_pools = [ cluster.node_pools for cluster.node_pools ]
             ]
 
     return cluster_dict
 
 
-def list_gke_node_pools(client, cluster):
-
-    request = container_v1.ListNodePoolsRequest(
-        #response = client.list_node_pools(parent="projects/netomedia2/locations/europe-west1-b/clusters/qa-karma-cluster/*")
-        parent=f"projects/{project}/locations/-/clusters/{cluster}/*"
-    )
-
-    # Make the request
-    response = client.list_node_pools(request=request)
-
-    # Handle the response
-    print(response)
-
-
 def main():
     print(list_gke_clusters(client))
-
-    for i in list_gke_clusters(client):
-        print(f"TEST - {i}")
 
 
 main()
